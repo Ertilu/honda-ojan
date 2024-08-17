e216afe30717af8c1b5b9dfda6d4e23437457f14
Reza Raka Nugraha <rezaraka00@gmail.com>
2024-08-17 21:59:23 +0700

    temporary navbar

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        primaryRed: "#E61B33",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
